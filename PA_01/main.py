47ef07832ddfd9d166fe0304146bd354e2905ef8
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2023-04-30 12:16:46 +0800

    done with exercise 1 first commit

--- PA_01/main.py
+++ PA_01/main.py
@@ -49,36 +49,36 @@ class MyGUI:
         
         self.textboxFrame.pack(padx=20)
         
-        #Exercise 2
-        self.textboxFrame2 = tk.Frame(self.root)
+        # #Exercise 2
+        # self.textboxFrame2 = tk.Frame(self.root)
         
-        self.label = tk.Label(self.root, text="Exercise 2", font=('Arial', 18), anchor='w', justify="left")
-        self.label.pack(padx=20, pady=20)
+        # self.label = tk.Label(self.root, text="Exercise 2", font=('Arial', 18), anchor='w', justify="left")
+        # self.label.pack(padx=20, pady=20)
         
-        self.label1 = tk.Label(self.root, text="Write a program that accepts string containing even a's and odd b's", font=('Arial', 14), anchor='w', justify="left")
-        self.label1.pack(padx=20, anchor="w")
+        # self.label1 = tk.Label(self.root, text="Write a program that accepts string containing even a's and odd b's", font=('Arial', 14), anchor='w', justify="left")
+        # self.label1.pack(padx=20, anchor="w")
         
-        self.textboxFrame1 = tk.Frame(self.root)
-        self.textboxFrame1.columnconfigure(0, weight=1)
-        self.textboxFrame1.columnconfigure(1, weight=1)
-        self.textboxFrame1.columnconfigure(2, weight=1)
-        self.textboxFrame1.columnconfigure(3, weight=1)
-        self.textboxFrame1.rowconfigure(0, weight=1)
-        self.textboxFrame1.rowconfigure(1, weight=1)
+        # self.textboxFrame1 = tk.Frame(self.root)
+        # self.textboxFrame1.columnconfigure(0, weight=1)
+        # self.textboxFrame1.columnconfigure(1, weight=1)
+        # self.textboxFrame1.columnconfigure(2, weight=1)
+        # self.textboxFrame1.columnconfigure(3, weight=1)
+        # self.textboxFrame1.rowconfigure(0, weight=1)
+        # self.textboxFrame1.rowconfigure(1, weight=1)
         
-        self.textbox2 = tk.Text(self.textboxFrame1, height=3, font=("Arial", 16))
-        self.textbox2.grid(row = 0, columnspan=2, sticky = tk.W+tk.E)
+        # self.textbox2 = tk.Text(self.textboxFrame1, height=3, font=("Arial", 16))
+        # self.textbox2.grid(row = 0, columnspan=2, sticky = tk.W+tk.E)
         
-        # button 1
-        self.button = tk.Button(self.textboxFrame1, text="Enter", font=("Arial",12), command=self.checkEven)
-        self.button.grid(row=1,column=0)
+        # # button 1
+        # self.button = tk.Button(self.textboxFrame1, text="Enter", font=("Arial",12), command=self.checkEven)
+        # self.button.grid(row=1,column=0)
         
-        #button 2
-        self.button = tk.Button(self.textboxFrame1, text="Clear", font=("Arial",12), command=self.clearWords1)
-        self.button.grid(row=1,column=1)
+        # #button 2
+        # self.button = tk.Button(self.textboxFrame1, text="Clear", font=("Arial",12), command=self.clearWords1)
+        # self.button.grid(row=1,column=1)
         
         
-        self.textboxFrame1.pack(padx=20)
+        # self.textboxFrame1.pack(padx=20)
         
         # Calculate the x and y coordinates of the top-left corner of the app
         x = (screen_width // 2) - (app_width // 2)
